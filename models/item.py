93f644201fa10f5ccf0b3885814d513a783db9a3
ipcampbell12 <ipcampbell12@gmail.com>
2022-12-15 16:54:39 -0800

    added database models

--- models/item.py
+++ models/item.py
@@ -1,6 +1,7 @@
 from db import db
 
 #sqlalchemy turns table rows into python objects
+#each item is associated with one store, but each item could have many items associated with it
 
 class ItemModel(db.Model):
     __tablename__ = "items"
@@ -8,4 +9,9 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80),unique=False, nullable=False)
     price = db.Column(db.Float(precision=2),unique=False, nullable=False)
-    store_id = db.Column(db.Intger, unique=False, nullable=False)
+    store_id = db.Column(db.Intger,db.ForeignKey('stores.id'), unique=False, nullable=False)
+
+    #store variable will get populated with StoreModel object, whose id matches the Foreign key
+    store = db.relationship("StoreModel",back_populates="items")
+
+    #I think the "store" is like a hypothetical column that gets added to this table
--- models/store.py
+++ models/store.py
@@ -6,4 +6,10 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80),unique=False, nullable=False)
-   
+
+    #the other end of the relationship with item
+    #needs to find all the items with store_id equal to this store's id
+    items = db.relationship("ItemModel", back_populates="store",lazy="dynamic")
+
+    #lazy=dynamic items will not be fetched from database until it is told to
+    #allows it a little faster, make request later
